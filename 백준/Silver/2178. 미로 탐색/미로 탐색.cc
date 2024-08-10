3e7a07aab557104ea14d07ec5847025e7340e600
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-08-10 15:21:21 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2236 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,7 +1,3 @@
-// 미로 최대 10,000칸 => O(N^2)까지 가능
-// (1, 1) -> (N, M)까지 최단 거리 => 오른쪽 아래로 계속 움직이면 됨 => 오른쪽 -> 아래 -> 왼쪽 -> 위 순서로 검사
-// 동시에 dist 배열에서 시작점에서부터 각 점까지의 거리를 저장 -> 마지막에 dist[n][m] + 1 출력 (출발점 포함)
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,7 +8,7 @@ int dy[4] = { 0, 1, 0, -1 };
 int dx[4] = { 1, 0, -1, 0 };
 queue<pair<int, int>> q;
 
-void BFS(int x, int y)
+void BFS()
 {
 	while (!q.empty())
 	{
@@ -25,7 +21,7 @@ void BFS(int x, int y)
 			// 새롭게 이동할 좌표 만들기
 			newX = currentPos.first + dx[i];
 			newY = currentPos.second + dy[i];
-	
+
 			if (newX < 0 || newX > m || newY < 0 || newY > n) continue; // 범위 벗어나는 지 확인
 
 			if (maze[newY][newX] && dist[newY][newX] == -1) // 이동할 수 있는 칸이고, 아직 거리를 계산하지 않았다면
@@ -34,10 +30,8 @@ void BFS(int x, int y)
 
 				// dist에 저장할 거리 계산
 				dist[newY][newX] = dist[currentPos.second][currentPos.first] + 1; // 인접한 점들까지의 거리 => 현재 위치까지의 거리 + 1
-			}		
+			}
 		}
-		
-		BFS(newX, newY); // 네 방향으로 이동할 수 있는 모든 점에 대해 거리를 계산한 후 큐의 맨 앞에 있는 곳으로 이동
 	}
 }
 
@@ -69,7 +63,7 @@ int main()
 		{
 			if (maze[y][x])
 			{
-				BFS(x, y);
+				BFS();
 			}
 		}
 	}

3961c042cf45cc1d6f11185de758aaa9e93e8b96
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-08-12 12:13:36 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long a, b, c;
-
-// A^B % C는 (A^(B/2) % C) * (A^(B/2) % C)로 표현 가능
-// A^(B/2) % C는 (A^(B/4) % C) * (A^(B/4) % C)로 표현 가능
-// A^(B/4) % C는 (A^(B/8) % C) * (A^(B/8) % C)로 표현 가능
+// b가 짝수인 경우
+// a^b mod c = {(a^(b/2) mod c) * (a^(b/2) mod c)} mod c
+// a^(b/2) mod c = {(a^(b/4) mod c) * (a^(b/4) mod c)} mod c
+// a^(b/4) mod c = {(a^(b/8) mod c) * (a^(b/8) mod c)} mod c
 // ...
-// A^2 % C는 (A % C) * (A % C)로 표현 가능
-// A % C는 바로 구할 수 있음 (B=1인 경우). 여기까지 구했으면 다시 위로 올라가면서 다 제곱 해줌
-// 이런식으로 가면 O(log B)로 계산 가능 (지수인 B가 계속 반으로 나누어지니까)
-long long func(long long a, long long b, long long c) {
-	if (b == 1) {
-		return a % c;
+// a^2 mod c = {(a mod c) * (a mod c)} mod c
+// a mod c => 바로 구할 수 있음 (b=1일때)
+
+// b가 홀수인 경우
+// a^b mod c = (a * a^(b-1)) mod c  (b-1은 짝수)
+//           = {(a mod c) * (a^(b-1) mod c)} mod c
+
+long long CalcPow(long long a, long long b, long long c) {
+	if (b == 1) { // base condition
+		return a % c; // a mod c 반환
 	}
 
-	long long val = func(a, b / 2, c);
-	val = (val * val) % c; // (A % C) * (A % C) = (A * A) % C
+	long long val = CalcPow(a, b / 2, c); // O(log b)
+	val = (val * val) % c; // val은 기본적으로 b가 짝수일때만 생각
 
-	if (b % 2 == 0) { 
+	if (b % 2 == 0) {
 		return val;
 	}
 	else {
-		// b가 홀수일 때 a^b는 a^(b-1) * a.    (b-1)은 짝수
-		// ex) A^3 % C = ((A % C) * (A % C)) * A = ((A * A) * A) % C
-		return (val * a) % c;
+		return ((a % c) * val) % c; // {(a mod c) * (a^(b-1) mod c)} mod c
 	}
 }
 
@@ -32,7 +33,8 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	long long a, b, c;
 	cin >> a >> b >> c;
 
-	cout << func(a, b, c);
+	cout << CalcPow(a, b, c);
 }

23f8d0ecfe73a8743286246ad5010b550afb5efe
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-08-16 17:58:59 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "  ___  ___  ___\n";
+    cout << "  | |__| |__| |\n";
+    cout << "  |           |\n";
+    cout << "   \\_________/\n";
+    cout << "    \\_______/\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |_____|\n";
+    cout << "  __/       \\__\n";
+    cout << " /             \\\n";
+    cout << "/_______________\\";
+}

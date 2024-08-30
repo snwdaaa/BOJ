969d773cf8338e4f8c67276053550d3a7c7dfead
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-08-30 13:06:47 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -8,29 +8,15 @@ int line[100001]; // 이동 거리를 저장할 배열
 void LineBFS() {
 	while (!q.empty()) {
 		auto x = q.front(); q.pop();
+		int nextPos[3] = { x - 1, x + 1, x * 2 };
 
 		// X-1, X+1, 2*X 모두 범위, 이미 방문한 곳인지, K와 같은 지 확인
 		for (int i = 0; i < 3; i++) {
-			switch (i) {
-				case 0: // x - 1
-					if (x - 1 < 0 || x - 1 >= 100001) break; // 범위 벗어나면
-					if (line[x - 1] != -1) break; // 이미 방문했으면
-					line[x - 1] = line[x] + 1; // x + 1은 걸린 시간
-					q.push(x - 1);
-					break;
-				case 1: // x + 1
-					if (x + 1 < 0 || x + 1 >= 100001) break; // 범위 벗어나면
-					if (line[x + 1] != -1) break; // 이미 방문했으면
-					line[x + 1] = line[x] + 1; // x + 1은 걸린 시간
-					q.push(x + 1);
-					break;
-				case 2: // 2 * x
-					if (x * 2 < 0 || x * 2 >= 100001) break; // 범위 벗어나면
-					if (line[x * 2] != -1) break; // 이미 방문했으면
-					line[x * 2] = line[x] + 1; // x + 1은 걸린 시간
-					q.push(x * 2);
-					break;
-			}
+			int nextIdx = nextPos[i];
+			if (nextIdx < 0 || nextIdx >= 100001) continue; // 범위
+			if (line[nextIdx] != -1) continue; // 방문 여부
+			line[nextIdx] = line[x] + 1;
+			q.push(nextIdx);
 		}
 
 		if (line[K] != -1) return; // 동생 위치 찾은 경우 종료

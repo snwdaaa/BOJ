043e344908980ed5105314c2f53c25cf3319d642
SeungWoo Lee <20211922@edu.hanbat.ac.kr>
2024-08-30 12:02:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,50 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int subinPos, brotherPos;
-int dist[100001]; // 일직선상에서 수빈이가 처음 위치에서 움직여서 동생을 찾을 때까지의 거리
-queue<int> q; // 다음으로 움직일 곳
+int N, K;
+queue<int> q;
+int line[100001]; // 이동 거리를 저장할 배열
 
-void BFS() {
+void LineBFS() {
 	while (!q.empty()) {
-		int currentPos = q.front(); q.pop();
+		auto x = q.front(); q.pop();
 
+		// X-1, X+1, 2*X 모두 범위, 이미 방문한 곳인지, K와 같은 지 확인
 		for (int i = 0; i < 3; i++) {
 			switch (i) {
-				case 0: // X - 1
-					if (currentPos - 1 < 0 || currentPos - 1 > 100000) break; // 범위
-					if (dist[currentPos - 1] != -1) break; // 이동하지 않은 곳으로만 이동
-					dist[currentPos - 1] = dist[currentPos] + 1;
-					q.push(currentPos - 1);
+				case 0: // x - 1
+					if (x - 1 < 0 || x - 1 >= 100001) break; // 범위 벗어나면
+					if (line[x - 1] != -1) break; // 이미 방문했으면
+					line[x - 1] = line[x] + 1; // x + 1은 걸린 시간
+					q.push(x - 1);
 					break;
-				case 1: // X + 1
-					if (currentPos + 1 < 0 || currentPos + 1 > 100000) break;
-					if (dist[currentPos + 1] != -1) break;
-					dist[currentPos + 1] = dist[currentPos] + 1;
-					q.push(currentPos + 1);
+				case 1: // x + 1
+					if (x + 1 < 0 || x + 1 >= 100001) break; // 범위 벗어나면
+					if (line[x + 1] != -1) break; // 이미 방문했으면
+					line[x + 1] = line[x] + 1; // x + 1은 걸린 시간
+					q.push(x + 1);
 					break;
-				case 2: // 2 * X
-					if (2 * currentPos < 0 || 2 * currentPos > 100000) break;
-					if (dist[2 * currentPos] != -1) break;
-					dist[2 * currentPos] = dist[currentPos] + 1;
-					q.push(2 * currentPos);
+				case 2: // 2 * x
+					if (x * 2 < 0 || x * 2 >= 100001) break; // 범위 벗어나면
+					if (line[x * 2] != -1) break; // 이미 방문했으면
+					line[x * 2] = line[x] + 1; // x + 1은 걸린 시간
+					q.push(x * 2);
 					break;
 			}
 		}
 
-		if (dist[brotherPos] != -1) return; // 동생 찾으면 종료
+		if (line[K] != -1) return; // 동생 위치 찾은 경우 종료
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cin >> subinPos >> brotherPos;
-	fill(dist, dist + 100001, -1); // -1로 초기화
+	cin >> N >> K;
 
-	dist[subinPos] = 0;
-	q.push(subinPos);
-	BFS();
-	
-	cout << dist[brotherPos];
+	fill(line, line + 100001, -1); // -1로 초기화
+
+	line[N] = 0;
+	q.push(N); // 수빈이의 첫 시작 위치를 큐에 추가
+
+	LineBFS();
+
+	cout << line[K];
 }
